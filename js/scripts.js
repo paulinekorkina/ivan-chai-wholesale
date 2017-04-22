8ce5b81fc59d2936cc1cf0bb96f53992dc346967
Polina <yuzha@inbox.ru>
2017-04-22 19:16:49 +0500

    Добавила скрытие меню при переходе по ссылке

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 $("input[type='number']").number();
+
+$(document).ready(function() {
+  var menu_show = $(".mobile-menu-btn");
+  var link = $(".menu-link");
+  var menu = $(".main-menu");
+
+  menu_show.click(function(event) {
+    event.preventDefault();
+    menu.toggleClass("main-menu-open");
+  });
+
+  link.click(function(event) {
+    menu.removeClass("main-menu-open")
+  });
+
+});
